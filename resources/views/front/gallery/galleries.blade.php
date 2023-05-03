c923fe52023f61c46baabbbb5b323a515d667c3f
stylzmedia2021 <stylzmedia2021@gmail.com>
2023-05-03 18:44:40 +0600

    Legal documents basic done

--- resources/views/front/gallery/galleries.blade.php
+++ resources/views/front/gallery/galleries.blade.php
@@ -1,63 +1,73 @@
 @extends('front.layouts.master')
-@php
-    $page_title='Media';
-@endphp
+
 @section('head')
     @include('meta::manager', [
-        'title' => 'Gallery - ' . ($settings_g['slogan'] ?? '')
+        'title' => $page->meta_title.' - ' . ($settings_g['title'] ?? env('APP_NAME')),
+        'description' => $page->meta_description,
+        'keywords' => $page->meta_tags,
+        'image' => $page->media_id ? $page->img_paths['original'] : null,
     ])
-    @php
-        $galleries = \App\Models\Gallery::orderBy('position', 'DESC')
-        ->active()
-        ->get();
-    @endphp
+
+@php
+    $galleries = App\Models\Gallery::paginate(12);
+@endphp
 
 @endsection
 
 @section('master')
-    <!-- Breadcrumb -->
-    <nav class="flex py-5 px-5 text-gray-700 bg-sky-400 border border-gray-200 dark:bg-gray-800 dark:border-gray-700" aria-label="Breadcrumb">
-        <div class="container">
-            <h2 class="text-white text-2xl font-semibold mb-5">{{$page_title}}</h2>
-            <ol class="inline-flex items-center space-x-1 md:space-x-3">
-                <li class="inline-flex items-center">
-                <a href="{{route('homepage')}}" class="inline-flex items-center text-sm font-medium text-white hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
-                    <svg class="mr-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
-                    Home
-                </a>
-                </li>
-                <li>
-                <div class="flex items-center">
-                    <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                    <a href="#" class="ml-1 text-sm font-medium text-white hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white">{{$page_title}}</a>
-                </div>
-                </li>
-            </ol>
+
+<!-- Breadcrumb -->
+    @php
+    if(empty($page->breadcrumb_background)){
+        $back_value="#2c3232b0";
+    }else{
+        $back_value=$page->breadcrumb_background;
+    }
+    if($page->is_color == 2){
+        $bg_bread="background:rgba(0, 0, 0, 0) url('../$back_value') no-repeat scroll center center / cover;";
+    }else{
+        $bg_bread="background:".$back_value;
+    }
+    @endphp
+    <div class="inner-banner">
+        <div class="inner-image" style="{{$bg_bread}}">
+            <img src="{{ $page->media_id ? $page->img_paths['original'] : null }}" alt="">
         </div>
-    </nav>
-    <!-- End Breadcrumb -->
-
-    {{-- gallery section start --}}
-    <section class="py-20 bg-gray-100">
         <div class="container">
-
-            <div class="mt-10 lg:flex lg:space-x-0 lg:flex-wrap">
+            <div class="inner-title text-center">
+                <h3>@if(empty($page->breadcrumb_title)){{$page->title}}@else{{$page->breadcrumb_title}}@endif</h3>
+                <ul>
+                    <li>
+                        <i class="flaticon-fireplace"></i>
+                    </li>
+                    <li>
+                        <a href="{{ route('homepage') }}">Home /</a>
+                    </li>
+                    <li>Page/</li>
+                    <li>{{$page->title}}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+<!-- Breadcrumb End-->
+<section class="galleries my-5">
+    <div class="container">
+            <div class="row justify-content-center">
                 @foreach ($galleries as $gallery)
-                    <div class="w-full lg:w-1/3 mb-5 pl-5 card">
-                        <div class="overflow-hidden ovelay">
-                            <img class="h-60 w-full object-cover" src="{{$gallery->img_paths['small']}}" alt="{{$gallery->name}}">
-                        </div>
-                        <div class="py-10 px-5 card-desc">
-                            {{-- <span class="text-xs text-white font-bold">{{date('d M, Y', strtotime($gallery->created_at))}}</span> --}}
-                            <h2 class="py-10 px-2 text-base font-medium text-white uppercase">{{$gallery->name}}</h2>
-                            <a class="text-sm text-sky-400 hover:text-sky-500" href="{{route('gallery.single',$gallery->name)}}">View Details <i class="fa fa-chevron-right"></i></a>
+                    <div class="col-md-6 col-lg-2 galleries-list">
+                        <div class="mb-4 galleries-item">
+                            <img class="galleries_img" src="{{ $gallery->img_paths['original'] }}" alt="{{$gallery->name}}">
+
+                            <div class="galleries-details">
+                            {{-- <h6 class="galleries-title">{{$gallery->name}}</h6> --}}
+                            <a href="{{route('gallery', $gallery->id)}}" class="button button_md">View All {{$gallery->name}}</a>
+                            </div>
                         </div>
                     </div>
                 @endforeach
             </div>
-        </div>
-    </section>
-@endsection
-@section('footer')
+        {{$galleries->links()}}
+    </div>
+</section>
 
 @endsection
--- resources/views/front/gallery/gallery.blade.php
+++ resources/views/front/gallery/gallery.blade.php
@@ -1,88 +1,75 @@
 @extends('front.layouts.master')
-@php
-    $page_title=$gallery->name;
-@endphp
+
 @section('head')
     @include('meta::manager', [
-        'title' => $gallery->name.' - ' . ($settings_g['slogan'] ?? '')
+        'title' => $gallery->name . ' - ' . ($settings_g['title'] ?? env('APP_NAME')),
+        'description' => $gallery->meta_description,
+        'keywords' => $gallery->meta_tags,
+        'image' => $gallery->media_id ? $page->img_paths['original'] : null,
     ])
 @endsection
-@section('master')
-     <!-- Breadcrumb -->
-     <nav class="flex py-5 px-5 text-gray-700 bg-sky-400 border border-gray-200 dark:bg-gray-800 dark:border-gray-700" aria-label="Breadcrumb">
-        <div class="container">
-            <h2 class="text-white text-2xl font-semibold mb-5">{{$page_title}}</h2>
-            <ol class="inline-flex items-center space-x-1 md:space-x-3">
-                <li class="inline-flex items-center">
-                <a href="{{route('homepage')}}" class="inline-flex items-center text-sm font-medium text-white hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
-                    <svg class="mr-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
-                    Home
-                </a>
-                </li>
-                <li>
-                <div class="flex items-center">
-                    <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                    <a href="#" class="ml-1 text-sm font-medium text-white hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white">{{$page_title}}</a>
-                </div>
-                </li>
-            </ol>
-        </div>
-    </nav>
-    <!-- End Breadcrumb -->
-    <section class="bg-gray-100 py-20">
-        <div class="container">
-            <div class="md:flex md:flex-wrap lg:flex lg:flex-wrap">
-                @foreach ($gallery->GalleryItems as $key => $item)
-                    <div class="w-full md:w-1/2 lg:w-1/3 mb-5 md:pl-5">
-                        @if ($item->image)
-                            <div class="overflow-hidden">
-                                <img class="h-52 w-full object-cover hover:scale-125 duration-1000" src="{{$item->img_paths['original']}}" alt="">
-                            </div>
-                            @if ($item->title)
-                                <div class="bg-white py-2 px-5">
-                                    <h2 class="py-2 text-base font-medium text-blue-900 uppercase">{{$item->title}}</h2>
-                                </div>
-                            @endif
 
-                        @elseif ($item->video_embade_code)
-                            <div class="overflow-hidden">
-                                {!!$item->video_embade_code!!}
-                            </div>
-                            @if ($item->title)
-                                <div class="bg-white py-2 px-5">
-                                    <h2 class="py-2 text-base font-medium text-blue-900 uppercase">{{$item->title}}</h2>
-                                </div>
-                            @endif
-                        @elseif ($item->video)
-                            <div class="overflow-hidden">
-                                <video class="h-52 w-full bg-white"  controls="controls" autoplay loop muted>
-                                    <source src="{{ asset($item->video) }}" type="video/mp4" />
-                                </video>
-                            </div>
-                            @if ($item->title)
-                                <div class="bg-white py-2 px-5">
-                                    <h2 class="py-2 text-base font-medium text-blue-900 uppercase">{{$item->title}}</h2>
-                                </div>
-                            @endif
-                            @elseif ($item->pdf_file)
-                            <div class="overflow-hidden h-52 w-full bg-white flex justify-center">
-                                <a href="{{ route('document.view', $item->title) }}">
-                                    <img class="h-52 w-52 object-cover hover:scale-125 duration-1000" src="{{asset('front/img/pdf.png')}}" alt="">
-                                </a>
-                            </div>
-                            @if ($item->title)
-                                <div class="bg-white py-2 px-5">
-                                    <a href="{{ route('document.view', $item->title) }}"><h2 class="py-2 text-base font-medium text-blue-900 uppercase">{{$item->title}}</h2></a>
-                                </div>
-                            @endif
-                        @endif
-                    </div>
-                @endforeach
+@section('master')
+
+<!-- Breadcrumb -->
+    @php
+    if(empty($gallery->breadcrumb_background)){
+        $back_value="#2c3232b0";
+    }else{
+        $back_value=$gallery->breadcrumb_background;
+    }
+    if($gallery->is_color == 2){
+        $bg_bread="background:rgba(0, 0, 0, 0) url('../$back_value') no-repeat scroll center center / cover;";
+    }else{
+        $bg_bread="background:".$back_value;
+    }
+    @endphp
+    <div class="inner-banner">
+        <div class="inner-image" style="{{$bg_bread}}">
+            <img src="{{ $gallery->img_paths['original'] }}" alt="">
+        </div>
+        <div class="container">
+            <div class="inner-title text-center">
+                <h3>@if(empty($gallery->breadcrumb_title)){{$gallery->name}}@else{{$gallery->breadcrumb_title}}@endif</h3>
+                <ul>
+                    <li>
+                        <i class="flaticon-fireplace"></i>
+                    </li>
+                    <li>
+                        <a href="{{ route('homepage') }}">Home /</a>
+                    </li>
+                    <li>Page/</li>
+                    <li>{{$gallery->name}}</li>
+                </ul>
             </div>
         </div>
-    </section>
+    </div>
+<!-- Breadcrumb End-->
 
+
+<div class="page_wrap">
+    <div class="container-md">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="mb-0">{{$gallery->name}}</h5>
+            </div>
+
+            <div class="card-body">
+                <div class="row">
+                    @foreach ($gallery->GalleryItems as $photo)
+                        <div class="col-md-6 col-lg-4">
+                            <a data-fslightbox href="{{$photo->img_paths['original']}}" class="d-block mb-3" style="border-radius: 4px;overflow: hidden;">
+                                <img src="{{$photo->img_paths['small']}}" alt="{{$gallery->gallery_name}}" class="whp">
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
-@section('footer')
 
+@section('footer')
+<script src="{{asset('front/js/fslightbox/fslightbox.js')}}"></script>
 @endsection
