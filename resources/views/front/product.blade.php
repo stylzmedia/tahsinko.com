7b85f30deb8c111130f2235e57bc284058fef0cc
stylzmedia2021 <stylzmedia2021@gmail.com>
2023-05-09 15:39:05 +0600

    breadcrumb included in all pages

--- resources/views/front/product.blade.php
+++ resources/views/front/product.blade.php
@@ -22,38 +22,7 @@
 
 @section('master')
 <!-- Breadcrumb -->
-    @php
-    if(empty($page->breadcrumb_background)){
-        $back_value="#2c3232b0";
-    }else{
-        $back_value=$page->breadcrumb_background;
-    }
-    if($page->is_color == 2){
-        $bg_bread="background:rgba(0, 0, 0, 0) url('../$back_value') no-repeat scroll center center / cover;";
-    }else{
-        $bg_bread="background:".$back_value;
-    }
-    @endphp
-    <div class="inner-banner">
-        <div class="inner-image" style="{{$bg_bread}}">
-            <img src="{{ $page->media_id ? $page->img_paths['original'] : null }}" alt="">
-        </div>
-        <div class="container">
-            <div class="inner-title text-center">
-                <h3>@if(empty($page->breadcrumb_title)){{$page->title}}@else{{$page->breadcrumb_title}}@endif</h3>
-                <ul>
-                    <li>
-                        <i class="flaticon-fireplace"></i>
-                    </li>
-                    <li>
-                        <a href="{{ route('homepage') }}">Home /</a>
-                    </li>
-                    <li>Page/</li>
-                    <li>{{$page->title}}</li>
-                </ul>
-            </div>
-        </div>
-    </div>
+@include('front.includes.breadcrumb')
 <!-- Breadcrumb End-->
 
     {{-- all-products --}}
